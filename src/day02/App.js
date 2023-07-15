cbf490d0388fe4d3558306bcd2df20ed725017d7
choiswan49 <evaswan429@gmail.com>
2023-07-15 17:05:02 +0900

    props, state, event

--- src/day02/App.js
+++ src/day02/App.js
@@ -4,14 +4,62 @@ import Section from "./component/sections/Section"
 import './App.css';
 import logo from '../logo.svg'
 
+/*
 const App = ()=>{
-    const textLogo = "React!!!"
+    const textLogo = "React!!!";
+    const today = new Date();
+
     return(
         <div>
             <Header logo={logo} textlogo={"React"}/>
-            {/* <Header logo={logo} textlogo={textLogo}/> */}
             <Section />
-            <Footer />
+            <Footer year={today.getFullYear()}/>
+        </div>
+    )
+}
+*/
+import {useState} from 'react';
+/*
+const App = ()=>{
+    const textLogo = "React!!!";
+    const today = new Date();
+    let num = 0; // 화면 갱신하지 못함
+    const [uiNum, setUiNum] = useState(0);  // 화면을 갱신하는 함수
+
+    setInterval(()=>{
+        num++;
+        console.log(num);
+
+        // setUiNum(uiNum=>uiNum+1);
+        setUiNum(uiNum+1);
+    },1000)
+    return(
+        <div>
+            <h1>hello {textLogo}</h1>
+            <div>{uiNum}</div>
+        </div>
+    )
+}
+*/
+const App = ()=>{
+    const textLogo = "React!!!";
+    const today = new Date();
+    let num = 0; // 화면 갱신하지 못함
+    const [uiNum, setUiNum] = useState(0);  // 화면을 갱신하는 함수
+
+    // document.querySelector('button').addEventListener('click', ()=>{
+    //     setUiNum(uiNum+1);
+    // })
+    
+    return(
+        <div>
+            <h1>hello {textLogo}</h1>
+            <div></div>
+            <button onClick={()=>setUiNum(uiNum+1)}>uiNum : {uiNum}</button>
+            <button onMouseOver={()=>console.log('mouseover')}
+                    onMouseOut={()=>console.log('mouseOut')}
+                    >마우스오버/아웃</button>
+
         </div>
     )
 }
--- src/day02/README.md
+++ src/day02/README.md
@@ -5,12 +5,15 @@ jsx : 새로운 언어 (javascript + xml:html)
 # 기존의 javascript, html과의 차이점
 1. class thrtjd => className
     태크, 컴포넌트 (우리가 만든 태그: 함수)
+
 2. 작업하면 자동 갱신
     HMR(Hot Module Replace) : 새로고침 없이 자동으로 모듈 바꿔주는 기능
     다양한 변수 사용법
+
     2-1. state : 컴포넌트 내에서 사용하는 변수, 화면 갱신하고 싶을 때 사용
     2-2. props : 속성, 변수로 전달
     2-3. context : props 전역으로 설정해서 사용
+
 3. hook : 함수
     useState
     useContext
@@ -34,3 +37,7 @@ rcc : 클래스형 컴포넌트
 # 크롬 브라우저에 설치하는 개발 도구
 react development tools
 각종 컴포넌트, state, props, context 확인할 수 있는 도구
+
+# react event handler
+1. <button onMouseOver>={파라미터가 없는 함수명}>마우스오버</button>
+2. <button onMouseOver>={()=>{함수(파라미터)}}>마우스오버</button>
